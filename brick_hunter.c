0fb3ccd7a83f835f8edd9bc3f4c621fff62aba34
Pranav Bhartiya <bhartiya.pranav@gmail.com>
2024-09-07 14:04:04 -0700

    new features added

--- brick_hunter.c
+++ brick_hunter.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <graphics.h>
 #include <conio.h>
-#include <dos.h>
 #include <stdlib.h>
 
 #define MOVE_LEFT 75
@@ -9,13 +8,18 @@
 #define ESC 27
 
 char ch;
+int lives = 3;  // Player starts with 3 lives
+int score = 0;  // Initial score
 
 void print_line();
 void print_line2();
 void messagebox();
 void winner();
+void update_score();
+void display_lives_and_score();
 
-void main() {
+void main()
+{
     int gd, gm;
     int IMG1, IMG2, IMG3, x, y, i, j, win = 0;
     void *BUFFER1, *BUFFER2, *BUFFER3;
@@ -25,26 +29,26 @@ void main() {
 
     gd = DETECT;
     initgraph(&gd, &gm, "C:\\turboc3\\bgi");
-
     cleardevice();
     setbkcolor(2);
 
     /* DRAWING BRICKS */
     rectangle(0, 0, 60, 22);
-    setfillstyle(1, 9);
+    setfillstyle(1, 9);  // Standard bricks
     floodfill(2, 2, 15);
 
     IMG1 = imagesize(0, 0, 60, 22);
     BUFFER1 = malloc(IMG1);
     getimage(0, 0, 60, 22, BUFFER1);
+
     putimage(0, 0, BUFFER1, XOR_PUT);
 
     /* PRINTNG BRICKS */
-    for (j = 0; j < 300; j += 30) {
-        for (i = 8; i < 600; i += 70) {
+    for (j = 0; j < 300; j += 30)
+        for (i = 8; i < 600; i += 70)
+        {
             putimage(i, 30 + j, BUFFER1, XOR_PUT);
         }
-    }
 
     print_line();
     print_line2();
@@ -54,7 +58,6 @@ void main() {
     rectangle(80, 445, 180, 460);
     setfillstyle(1, 15);
     floodfill(82, 447, 6);
-
     IMG2 = imagesize(80, 445, 180, 460);
     BUFFER2 = malloc(IMG2);
     getimage(80, 445, 180, 460, BUFFER2);
@@ -64,13 +67,15 @@ void main() {
     setfillstyle(1, 12);
     circle(350, 350, 10);
     floodfill(350, 350, WHITE);
-
     IMG3 = imagesize(340, 340, 360, 360);
     BUFFER3 = malloc(IMG3);
     getimage(340, 340, 360, 360, BUFFER3);
     putimage(340, 340, BUFFER3, XOR_PUT);
 
-    for (;;) {
+    while (lives > 0)  // Run the game as long as the player has lives
+    {
+        display_lives_and_score();  // Display lives and score at the top
+
         putimage(x_axis, y_axis, BUFFER3, XOR_PUT);
         delay(speed);
         putimage(x_axis, y_axis, BUFFER3, XOR_PUT);
@@ -78,33 +83,46 @@ void main() {
         x_axis = x_axis + (x_direction * 2);
         y_axis = y_axis + (y_direction * 3);
 
-        if (x_axis + 10 - 1 > 640) {
+        if (x_axis + 10 - 1 > 640)
+        {
             x_direction *= -1;
             x_axis = 640 - 10 + 1;
         }
-        if (x_axis < 0) {
+
+        if (x_axis < 0)
+        {
             x_direction *= -1;
             x_axis = 0;
         }
-        if (y_axis + 10 - 1 > 470) {
+
+        if (y_axis + 10 - 1 > 470)
+        {
             cleardevice();
             initgraph(&gd, &gm, "C:\\turboc3\\bgi");
-            outtextxy(200, 200, "You lose :(");
-            free(BUFFER1);
-            free(BUFFER2);
-            free(BUFFER3);
-            delay(2000);
-            messagebox();
+            outtextxy(200, 200, "You lost a life!");
+            delay(1000);
+            lives--;  // Decrease lives when the ball is lost
+            x_axis = 325;
+            y_axis = 325;
+            if (lives <= 0)
+            {
+                cleardevice();
+                outtextxy(200, 200, "Game Over!");
+                delay(2000);
+                break;
+            }
         }
 
-        if (getpixel(x_axis, y_axis) == 6) {
+        if (getpixel(x_axis, y_axis) == 6)  // Paddle collision
+        {
             y_direction *= -1;
             sound(900);
             delay(15);
             nosound();
         }
 
-        if (getpixel(x_axis, y_axis) == 9) {
+        if (getpixel(x_axis, y_axis) == 9)  // Brick collision
+        {
             sound(300);
             delay(50);
             nosound();
@@ -115,33 +133,36 @@ void main() {
             y = (y / 30) * 30;
             putimage(x, y, BUFFER1, XOR_PUT);
             win++;
+            score += 100;  // Increase score when a brick is hit
+            update_score();  // Update score display
 
-            if (win == 40) {
+            if (win == 40)
+            {
                 winner();
             }
         }
 
-        if (y_axis < 0) {
+        if (y_axis < 0)
+        {
             y_direction *= -1;
             y_axis = 0;
         }
 
-        if (kbhit()) {
+        if (kbhit())
+        {
             ch = getch();
-
-            if (ch != ESC) {
+            if (ch != ESC)
+            {
                 putimage(80 + X_POSITION, 445 + Y_POSITION, BUFFER2, XOR_PUT);
-
                 if (ch == MOVE_LEFT)
                     X_POSITION += -40;
-
                 if (ch == MOVE_RIGHT)
                     X_POSITION += 40;
-
                 putimage(80 + X_POSITION, 445 + Y_POSITION, BUFFER2, XOR_PUT);
             }
 
-            if (ch == ESC) {
+            if (ch == ESC)
+            {
                 /* FREE MEMORY OCCUPIED BY IMAGES */
                 free(BUFFER1);
                 free(BUFFER2);
@@ -153,7 +174,8 @@ void main() {
     }
 }
 
-void print_line() {
+void print_line()
+{
     setcolor(WHITE);
     line(0, 18, 640, 18);
     line(0, 19, 640, 19);
@@ -162,7 +184,8 @@ void print_line() {
     line(0, 22, 640, 22);
 }
 
-void print_line2() {
+void print_line2()
+{
     line(0, 465, 640, 465);
     line(0, 466, 640, 466);
     line(0, 467, 640, 467);
@@ -170,58 +193,29 @@ void print_line2() {
     line(0, 469, 640, 469);
 }
 
-void messagebox() {
-    cleardevice();
-
-    /* Draw a rectangle */
+void display_lives_and_score()
+{
+    char text[50];
     setcolor(WHITE);
-    rectangle(179, 109, 457, 207);
-
-    /* Draw rest of the rectangle */
-    rectangle(178, 108, 458, 208);
-    rectangle(177, 107, 459, 209);
-    rectangle(176, 106, 460, 210);
-    rectangle(175, 105, 461, 211);
-    rectangle(174, 104, 462, 212);
-
-    settextstyle(11, 0, 1);
-    nosound();
-    setcolor(12);
-
-    /* Output a message */
-    outtextxy(222, 140, "DO YOU WANT TO PLAY AGAIN");
-
-    setcolor(11);
-    outtextxy(229, 155, "PRESS [ Y ] OR [ N ]!");
-
-    ch = getche();
-
-    if (ch == 'y') {
-        main();
-    } else {
-        nosound();
-        exit(0);
-    }
+    sprintf(text, "Lives: %d | Score: %d", lives, score);
+    outtextxy(10, 10, text);  // Show lives and score at the top
 }
 
-void winner() {
+void update_score()
+{
+    setcolor(WHITE);
+    char score_text[20];
+    sprintf(score_text, "Score: %d", score);
+    outtextxy(500, 10, score_text);
+}
+
+void winner()
+{
     cleardevice();
     setbkcolor(0);
-
-    /* Draw a rectangle */
     setcolor(WHITE);
     rectangle(179, 109, 457, 207);
-
-    /* Draw rest of the rectangle */
-    rectangle(178, 108, 458, 208);
-    rectangle(177, 107, 459, 209);
-    rectangle(176, 106, 460, 210);
-    rectangle(175, 105, 461, 211);
-    rectangle(174, 104, 462, 212);
-
-    setcolor(14);
-    outtextxy(222, 140, "Hey You Win the Game!");
-
+    outtextxy(222, 140, "Hey You Win the Game !");
     sound(3333);
     delay(999);
     nosound();
